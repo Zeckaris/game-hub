96ee6678dc09e560df6a1129d063f993d539d4a0
Amanuel <amanuelfisseha2@gmail.com>
2025-02-06 16:00:36 +0300

    Created Navbar and made the aside component dynamic

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,32 @@
 import { useState } from "react";
-
+import { Button, Grid, GridItem, HStack } from "@chakra-ui/react";
 import "./App.css";
+import NavBar from "./components/NavBar";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Grid
+        templateAreas={{ base: `"nav" "main"`, lg: `"nav nav" "aside main"` }}
+      >
+        <GridItem area={"nav"}>
+          <NavBar />
+        </GridItem>
+
+        <GridItem
+          area={"aside"}
+          bg={"red"}
+          display={{ base: "none", lg: "block" }}
+        >
+          Aside
+        </GridItem>
+
+        <GridItem area={"main"} bg={"orange"}>
+          Main
+        </GridItem>
+      </Grid>
+    </>
+  );
 }
 
 export default App;
