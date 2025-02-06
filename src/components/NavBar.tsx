a22060914abd011e2dfe4b89aac6a84e03e2794e
Amanuel <amanuelfisseha2@gmail.com>
2025-02-06 16:33:43 +0300

    added theme mode switch from chackra ui

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,12 +1,14 @@
 import { HStack, Image } from "@chakra-ui/react";
 import React from "react";
 import logo from "../assets/logo.webp";
+import { ColorModeButton } from "./ui/color-mode";
 
 function NavBar() {
   return (
     <div>
       <HStack>
         <Image src={logo} boxSize={"60px"} />
+        <ColorModeButton />
       </HStack>
     </div>
   );
--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,16 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "./components/ui/provider";
+import { ColorModeProvider } from "./components/ui/color-mode";
 import "./index.css";
 import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <Provider>
-    <StrictMode>
-      <App />
-    </StrictMode>
+    <ColorModeProvider>
+      <StrictMode>
+        <App />
+      </StrictMode>
+    </ColorModeProvider>
   </Provider>
 );
