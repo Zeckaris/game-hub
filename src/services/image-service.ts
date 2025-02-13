4b43f3d00c571b23287943898d9e20029c3c23e5
Amanuel <amanuelfisseha2@gmail.com>
2025-02-13 10:16:27 +0300

    Handled games with no image by placing a broken image picture in the assets

--- src/services/image-service.ts
+++ src/services/image-service.ts
@@ -1,6 +1,8 @@
+import noImage from '../assets/no-image-placeholder-6f3882e0.webp';
+
 const getCropedImage= (url:string)=>{
     if(!url){
-        return url;
+        return noImage;
     }
    const index= url.indexOf("media/") + "media/".length;
    const newUrl= url.slice(0,index) +"crop/600/400/"+ url.slice(index);
